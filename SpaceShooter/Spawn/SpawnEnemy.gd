9d2f7596c0c9a77356cea96700ed21c3a814b3bc
Manu <manu9br6@gmail.com>
2022-12-11 17:50:11 +0100

    Spawn enemigos

---
+++ SpaceShooter/Spawn/SpawnEnemy.gd
@@ -0,0 +1,29 @@
+extends Node
+
+
+const enemies = [
+	preload("res://enemy/EnemyBlue.tscn"),
+	preload("res://enemy/EnemyRed.tscn")
+]
+
+func _ready():
+	$SpawnTimer.start()
+
+func spawn():
+	var enemy = Utils.choice_list(enemies).instance()
+	var pos = Vector2()
+	
+	randomize()
+	pos.x = rand_range(64, Utils.view_size.x)
+	pos.y -64
+	enemy.position = pos
+	
+	add_child(enemy)
+	$SpawnTimer.wait_time = rand_range(0.5, 2.0)
+	
+	
+	
+
+
+func _on_SpawnTimer_timeout():
+	spawn()
